c10789264e9008ae19ab0774fdcb3f656d5f824f
Weldameshi <47304615+Weldameshi@users.noreply.github.com>
2020-01-22 14:40:41 -0700

    board and piece

--- nim/src/main/java/model/Board.java
+++ nim/src/main/java/model/Board.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Board {
+	private Piece[][] board;
+	
+	Board(int x, int y){
+		
+	}
 
+	public Piece[][] getBoard() {
+		return board;
+	}
+
+	public void setBoard(Piece[][] board) {
+		this.board = board;
+	}
+
+	
+	public String toString() {
+		String boardString = "";
+		int counter = 0;
+		return null;
+	}
 }
--- nim/src/main/java/model/Piece.java
+++ nim/src/main/java/model/Piece.java
@@ -2,4 +2,24 @@ package model;
 
 public class Piece {
 
+	private boolean isActive = true;
+	
+	Piece(){
+		
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	public String toString() {
+		return null;
+		
+	}
+	
+	
 }
