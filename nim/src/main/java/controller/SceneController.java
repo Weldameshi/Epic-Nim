0a686672001354780f6e7dd3304b3f3f0c81c89a
VegaNan <47089431+VegaNan@users.noreply.github.com>
2020-01-22 15:00:07 -0700

    raw view controllers done

--- nim/src/main/java/controller/SceneController.java
+++ nim/src/main/java/controller/SceneController.java
@@ -1,5 +1,33 @@
 package controller;
 
-public class SceneController {
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class SceneController{
+
+	public static Stage primaryStage;
+	
+	public void changeScene(String fxmlName, String cssName){
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlName));
+			AnchorPane root;
+			root = (AnchorPane) loader.load();
+			
+			//TODO set this size
+			Scene scene = new Scene(root, 450, 700);
+			scene.getStylesheets().add(cssName);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
 
 }
--- nim/src/main/java/controller/ViewController.java
+++ nim/src/main/java/controller/ViewController.java
@@ -1,5 +1,36 @@
 package controller;
 
-public class ViewController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+public class ViewController extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//TODO set the stuff to the main page
+			String css ="";
+			String fxml = "";
+			
+			SceneController.primaryStage = primaryStage;
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+			AnchorPane root = (AnchorPane) loader.load();
+			
+			//TODO set this size
+			Scene scene = new Scene(root, 450, 700);
+			scene.getStylesheets().add(css);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
